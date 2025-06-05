85506c3b4e4c0e37fe94b08cdf1f1158df903312
sulocha88 <sulochayatageri@gmail.com>
2025-06-05 07:15:01 +0530

    message

---
+++ student_marks.java
@@ -0,0 +1,25 @@
+//student_course.java
+class Course{
+    String course;
+    int sem;
+    float marks;
+
+    Course(String course, int sem, float marks){
+        this.course=course;
+        this.sem=sem;
+        this.marks=marks;
+    }
+    void displayCourse(){
+        System.out.println("Course:"+course+" sem:"+sem+" Marks:"+marks);
+    }
+}
+public class student_marks {
+    public static void main(String[] args) {
+        Course c = new Course("Java",4,40);
+        Course c1 = new Course("Big Data",4,40);
+        Course c2 = new Course("Devops",4,40);
+        c.displayCourse();
+        c1.displayCourse();
+        c2.displayCourse();
+    }
+}
